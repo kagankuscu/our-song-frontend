a61717bd09ae223e181297aa699208556f97086a
kagankuscu <kagan-kuscu@hotmail.com.tr>
2022-10-13 10:11:11 +0300

    New Route added

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,15 @@ import { HomePage } from './pages/HomePage';
 import './App.css';
 import { NavBarComponent } from './components/navbar/NavBarComponent';
 import 'bootstrap/dist/css/bootstrap.css';
-import { AllSongPage } from './pages/AllSongPage';
-import { SongPage } from './pages/SongPage';
-import { UpdatePage } from './pages/UpdatePage';
-import { AddSongPage } from './pages/AddSongPage';
-import { RandomPage } from './pages/RandomPage';
-import { NotFound } from './components/404/NotFound';
+import {
+  AllSongPage,
+  SongPage,
+  UpdatePage,
+  AddSongPage,
+  RandomPage,
+  NotFound,
+  PlayerPage,
+} from './pages/Pages';
 
 function App() {
   return (
@@ -20,6 +23,7 @@ function App() {
         <Route path="/song/:id" element={<SongPage />} />
         <Route path="/addsong" element={<AddSongPage />} />
         <Route path="/randomsong" element={<RandomPage />} />
+        <Route path="/player/:id" element={<PlayerPage />} />
         <Route path="/updatesong/:id" element={<UpdatePage />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
