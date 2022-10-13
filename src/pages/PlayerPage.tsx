bda708bb18c18bcd923ba4bc78fd02fe4d109da8
kagankuscu <kagan-kuscu@hotmail.com.tr>
2022-10-13 10:12:30 +0300

    New Page

---
+++ src/pages/PlayerPage.tsx
@@ -0,0 +1,10 @@
+import { useParams } from 'react-router-dom';
+import { Loading } from '../components/Loading/Loading';
+import { Player } from '../components/Player/Player';
+import useFetchById from '../hooks/useFetch';
+
+export const PlayerPage: React.FC = () => {
+  const { id } = useParams();
+  const [song, loading] = useFetchById(`${id!}`);
+  return <>{loading ? <Loading /> : <Player song={song!} />}</>;
+};
