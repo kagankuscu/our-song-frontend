bd348bc62dae5d88f99dceabe4758d8dd438df95
kagankuscu <kagan-kuscu@hotmail.com.tr>
2022-10-13 10:11:50 +0300

    Player component

---
+++ src/components/Player/Player.tsx
@@ -0,0 +1,21 @@
+import { ISong } from '../../interface/Song/ISong';
+import { AudioPlayer } from '../AudioPlayer/AudioPlayer';
+import { SongInfo } from '../SongInfo/SongInfo';
+
+export const Player: React.FC<ISong> = ({ song }) => {
+  return (
+    <div
+      className="container"
+      style={{
+        height: 'calc(100% - 116px)',
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '1rem',
+        justifyContent: 'center',
+      }}
+    >
+      <SongInfo song={song} />
+      <AudioPlayer song={song.Url} />
+    </div>
+  );
+};
