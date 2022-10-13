b082436d89df9f95ed8caa4d16dd5425d1c699dc
kagankuscu <kagan-kuscu@hotmail.com.tr>
2022-10-13 10:10:35 +0300

    import file

---
+++ src/pages/Pages.ts
@@ -0,0 +1,9 @@
+import { AllSongPage } from './AllSongPage';
+import { SongPage } from './SongPage';
+import { UpdatePage } from './UpdatePage';
+import { AddSongPage } from './AddSongPage';
+import { RandomPage } from './RandomPage';
+import { NotFound } from '../components/404/NotFound';
+import { PlayerPage } from './PlayerPage';
+
+export { AllSongPage, SongPage, UpdatePage, AddSongPage, RandomPage, NotFound, PlayerPage }
