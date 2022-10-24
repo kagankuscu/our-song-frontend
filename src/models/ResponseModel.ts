aa83776949a970f437b38aba945f50d7ca4868db
kagankuscu <kagan-kuscu@hotmail.com.tr>
2022-10-24 18:23:19 +0300

    Random Page Error is fix.
    Use effect does work after React 18 twice on React.StrictMode that why i use to runOnce variable run once.

--- src/models/ResponseModel.ts
+++ src/models/ResponseModel.ts
@@ -1,7 +1,14 @@
 import { PaginateModel } from "./PaginateModel";
+import { Song } from "./SongModel";
 
 export interface ResponseModel {
     lenght: number,
     result: PaginateModel,
+    status: number
+}
+
+export interface ResponseModelSong {
+    lenght: number,
+    result: Song,
     status: number
 }
